7d9a41883ccda3df9b1898a8c57e3277ed3def8a
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-18 19:56:49 +0300

    configure users databases and register route

--- main.py
+++ main.py
@@ -8,12 +8,31 @@ from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
+from forms import RegisterForm
 import os
 import smtplib
 
-
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+ckeditor = CKEditor
 Bootstrap5(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
@@ -21,6 +40,12 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
