f245ed3d2ffd1bf06fb9b00ddea733acdaebebd1
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-18 19:56:06 +0300

    create forms.py

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms.validators import DataRequired, URL
+
+
+class RegisterForm(FlaskForm):
+    fname = StringField("First Name", validators=[DataRequired()])
+    lname = StringField("Last Name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
