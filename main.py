b364fdfbb02ba9b67db8d838b4fecd249c79631e
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-18 18:45:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from datetime import date, datetime
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
+import os
+import smtplib
+
+
+app = Flask(__name__)
+Bootstrap5(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
